59b51c5b872401c07efa4b6818bfa21ceed20530
stih01 <inoventica@bk.ru>
2025-03-13 15:23:36 +0700

    Update twrp_PH1M_EA_9970A.mk

--- twrp_PH1M_EA_9970A.mk
+++ twrp_PH1M_EA_9970A.mk
@@ -18,7 +18,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit from PH1M_EA_9970A device
-$(call inherit-product, device/tpv/PH1M_EA_9970A/device.mk)
+$(call inherit-product, device/Philips/PH1M_EA_9970A/device.mk)
 
 PRODUCT_DEVICE := PH1M_EA_9970A
 PRODUCT_NAME := twrp_PH1M_EA_9970A
