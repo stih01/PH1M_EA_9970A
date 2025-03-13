9264c400c0e40e1998ba4e35927357ee0ebc3fe4
stih01 <inoventica@bk.ru>
2025-03-13 17:02:31 +0700

    Update twrp_PH1M_EA_9970A.mk

--- twrp_PH1M_EA_9970A.mk
+++ twrp_PH1M_EA_9970A.mk
@@ -6,16 +6,15 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+#$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit some common Twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Устанавливает ключи gsi в ramdisk, чтобы загрузить GSI разработчика с проверенной загрузкой.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
-
-# Наследовать от этих продуктов. Сначала самое конкретное.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_minimal.mk)
 
 # Inherit from PH1M_EA_9970A device
 $(call inherit-product, device/Philips/PH1M_EA_9970A/device.mk)
