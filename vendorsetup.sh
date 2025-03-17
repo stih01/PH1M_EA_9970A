19c2348f1e5deb6e1bea476514e4911dfc1b0330
stih01 <inoventica@bk.ru>
2025-03-17 17:28:43 +0700

    Create vendorsetup.sh

---
+++ vendorsetup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+cd ./vendor/twrp/config/
+sed -i '/^ifeq/,$d' BoardConfigTWRP.mk
+cd ~-
+
+add_lunch_combo omni_PH1M_EA_9970A-user
+add_lunch_combo omni_PH1M_EA_9970A-userdebug
+add_lunch_combo omni_PH1M_EA_9970A-eng
