5c4e02da44c62ca27af689b2174ae3d9cfaaaad7
stih01 <inoventica@bk.ru>
2025-03-17 19:40:06 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,6 @@ cd ./vendor/twrp/config/
 sed -i '/^ifeq/,$d' BoardConfigTWRP.mk
 cd ~-
 
-add_lunch_combo omni_PH1M_EA_9970A-user
-add_lunch_combo omni_PH1M_EA_9970A-userdebug
-add_lunch_combo omni_PH1M_EA_9970A-eng
+add_lunch_combo twrp_PH1M_EA_9970A-user
+add_lunch_combo twrp_PH1M_EA_9970A-userdebug
+add_lunch_combo twrp_PH1M_EA_9970A-eng
