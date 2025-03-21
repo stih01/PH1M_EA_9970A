3cbfa1caca4b715890bace62d11877d57118dca4
stih01 <inoventica@bk.ru>
2025-03-21 15:36:42 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,6 +9,13 @@ cd ./vendor/twrp/config/
 sed -i '/^ifeq/,$d' BoardConfigTWRP.mk
 cd ~-
 
+cd ./bootable/recovery/
+sed -i '2628 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g' partitionmanager.cpp
+sed -i '2629 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g' partitionmanager.cpp
+sed -i '2684 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g' partitionmanager.cpp
+sed -i '2685 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g' partitionmanager.cpp
+cd ~-
+
 cd ./bootable/recovery/gui/theme/common/languages
 sed -i '119 s/Перезагрузка/Rebooting/g' ru.xml
 cd ~-
