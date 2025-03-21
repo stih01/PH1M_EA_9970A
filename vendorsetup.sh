d48c5cd4850d8212e92dc6ccba88950ffeb40ab9
stih01 <inoventica@bk.ru>
2025-03-21 15:42:34 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,10 +10,10 @@ sed -i '/^ifeq/,$d' BoardConfigTWRP.mk
 cd ~-
 
 cd ./bootable/recovery/
-sed -i '2628 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g' partitionmanager.cpp
-sed -i '2629 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g' partitionmanager.cpp
-sed -i '2684 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g' partitionmanager.cpp
-sed -i '2685 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g' partitionmanager.cpp
+sed -i '2628 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g
+        2629 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g
+        2684 s/sys\/class\/android_usb\/android0\/idVendor/config\/usb_gadget\/g1\/idVendor/g
+        2685 s/sys\/class\/android_usb\/android0\/idProduct/config\/usb_gadget\/g1\/idProduct/g' partitionmanager.cpp
 cd ~-
 
 cd ./bootable/recovery/gui/theme/common/languages
